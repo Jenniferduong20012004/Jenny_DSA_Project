50fc76bcb4ea5f7a4b82e7a7e3b816664b5f08ce
Itsjennyyahh <56812916+Jenniferduong20012004@users.noreply.github.com>
2024-04-03 20:16:53 +0700

    add base

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -1,6 +1,8 @@
 package Main;
 
-public class GamePanel {
+import javax.swing.*;
+
+public class GamePanel extends JPanel {
     private Game game;
     public GamePanel (Game game){
         this.game = game;
--- src/Main/GameWindow.java
+++ src/Main/GameWindow.java
@@ -1,4 +1,17 @@
 package Main;
 
-public class GameWindow {
+import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+
+
+public class GameWindow extends JFrame {
+    public GameWindow (GamePanel gamePanel){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(gamePanel);
+        setResizable(false);
+        pack();
+        setVisible(true);
+
+    }
 }
